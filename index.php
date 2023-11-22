420c6592060e3ae2867f2da7a529d10520e380dc
Nital <daniel.siwiaszczyk0@gmail.com>
2023-11-23 00:57:59 +0100

    Implement simple CSS

--- index.php
+++ index.php
@@ -32,6 +32,12 @@ if(isset($_POST['Sub'])){
     if(!empty($value_hex)) {$hex = true; $input_type = 4; $value = $value_hex;}
 
     exec("calculator.exe $value $input_type", $output, $retval);
+
+    $value_bin=$output[0];
+    $value_oct=$output[1];
+    $value_dec=$output[2];
+    $value_hex=$output[3];
+
 }?>
 
 
@@ -44,45 +50,47 @@ if(isset($_POST['Sub'])){
     <head>
         <title>Numeral system calculator</title>
         <meta Charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
     
-    
-    <form action="index.php" method="post">
+    <div id="center">
+        <center>
+        <div id="valign"> 
+            <h1>Numeral system converter</h1>
 
-    Bin: <input type="text" name="Bin"
-    placeholder=<?php 
-    if(empty($value_bin)) {Echo("00000000000000000000000000000000");}
-    else {Echo($value_bin);}?>
-    <?php if($oct||$dec||$hex) {Echo("Disabled");} ?>><br>
+            <form action="index.php" method="post">
 
-    Oct: <input type="text" name="Oct"
-    placeholder=<?php
-    if(empty($value_oct)) {Echo("00000000000");}
-    else {Echo($value_oct);}?>
-    <?php if($bin||$dec||$hex) {Echo("Disabled");} ?>><br>
+            Bin: <input type="text" name="Bin"
+            placeholder=<?php 
+            if(empty($value_bin)) {Echo("00000000000000000000000000000000");}
+            else {Echo($value_bin);}?>
+            <?php if($oct||$dec||$hex) {Echo("Disabled");} ?>><br>
 
-    Dec: <input type="text" name="Dec"
-    placeholder=<?php
-    if(empty($value_dec)) {Echo("0000000000");}
-    else {Echo($value_dec);}?>
-    <?php if($bin||$oct||$hex) {Echo("Disabled");} ?>><br>
+            Oct: <input type="text" name="Oct"
+            placeholder=<?php
+            if(empty($value_oct)) {Echo("00000000000");}
+            else {Echo($value_oct);}?>
+            <?php if($bin||$dec||$hex) {Echo("Disabled");} ?>><br>
 
-    Hex: <input type="text" name="Hex"
-    placeholder=<?php
-    if(empty($value_hex)) {Echo("00000000");}
-    else {Echo($value_hex);}?>
-    <?php if($bin||$oct||$dec) {Echo("Disabled");} ?>><br>
+            Dec: <input type="text" name="Dec"
+            placeholder=<?php
+            if(empty($value_dec)) {Echo("0000000000");}
+            else {Echo($value_dec);}?>
+            <?php if($bin||$oct||$hex) {Echo("Disabled");} ?>><br>
 
-    <input type="submit" name="Sub">
-    </form>
-    
+            Hex: <input type="text" name="Hex"
+            placeholder=<?php
+            if(empty($value_hex)) {Echo("00000000");}
+            else {Echo($value_hex);}?>
+            <?php if($bin||$oct||$dec) {Echo("Disabled");} ?>><br>
 
-    
-    <?php
-    //printing out resault of C script
-    print_r($output);
-    ?>
+            <input type="submit" name="Sub">
+            </form> 
+        
+        </div>
+        </center>
+    </div>
 
     </body>
 </html>
