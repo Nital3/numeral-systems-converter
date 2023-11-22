1309e9b4a7792297ed0307158764b60c726186da
Nital <daniel.siwiaszczyk0@gmail.com>
2023-11-22 09:45:21 +0100

    Conversion optimalization
    Now program given number in one system, calculates it to remaining 3 systems and returns all 4 numbers to GUI

--- calculator.c
+++ calculator.c
@@ -19,101 +19,148 @@ char hexadecimal_char[9];
 //argv - table of arguments, each cell contains string pointer, argv[0] == name of program
 //List of argument passed from GUI:
 //argv[0] = name of program
-//argv[1] = number in binary system as table of chars
-//argv[2] = number in octal system as table of chars
-//argv[3] = number in decimal system as table of chars
-//argv[4] = number in hexadecimal system as table of chars
-int main(int argc, char* argv[]){
+//argv[1] = value
+//argv[2] = system of given number (1 - Binary, 2 - Octal, 3 - Deciamal, 4 - Hexadecimal)
+void main(int argc, char* argv[]){
 
+    char convertion = '0';
+    memcpy(&convertion,argv[2],2);
+    int data_type = char_to_long(convertion);
+    long final_decimal_long = 0;
 
-
-    //Binary to decimal convertion
-    //Max binary lenght: 32 digits
-    long binary_long[33];
-    strcpy(binary_char,argv[1]);
+    switch (data_type)
+    {
+    case 1:
+        //Binary to decimal convertion
+        //Max binary lenght: 32 digits
+        long binary_long[33];
+        strcpy(binary_char,argv[1]);
     
-    //converting character binary table into long binary table
-    for(int i=0; i<strlen(binary_char); i++){
-        binary_long[i] = char_to_long(binary_char[i]);
-    }
+        //converting character binary table into long binary table
+        for(int i=0; i<strlen(binary_char); i++){
+            binary_long[i] = char_to_long(binary_char[i]);
+        }
 
-    //converting binary long table into single decimal long
-    long bdecimal_long = nsys_to_decimal(binary_long, strlen(binary_char), 2);
+        //converting binary long table into single decimal long
+        final_decimal_long = nsys_to_decimal(binary_long, strlen(binary_char), 2);
+        break;
 
-
-
-    //Octal tp decimal conversion
-    //Max Octal lenght: 11
-    long octal_long[12];
-    strcpy(octal_char,argv[2]);
+    case 2:
+        //Octal tp decimal conversion
+        //Max Octal lenght: 11
+        long octal_long[12];
+        strcpy(octal_char,argv[1]);
     
-    //converting character octal table into long octal table
-    for(int i=0; i<strlen(octal_char); i++){
-        octal_long[i] = char_to_long(octal_char[i]);
-    }
+        //converting character octal table into long octal table
+        for(int i=0; i<strlen(octal_char); i++){
+            octal_long[i] = char_to_long(octal_char[i]);
+        }
 
-    //converting octal long table into single decimal long
-    long odecimal_long = nsys_to_decimal(octal_long, strlen(octal_char), 8);
+        //converting octal long table into single decimal long
+        final_decimal_long = nsys_to_decimal(octal_long, strlen(octal_char), 8);
+        break;
 
-
-    //decimal char to long conversion
-    //Max decimal lenght: 10
-    long decimal_long[11];
-    strcpy(decimal_char,argv[3]);
+    case 3:
+        //decimal char to long conversion
+        //Max decimal lenght: 10
+        long decimal_long[11];
+        strcpy(decimal_char,argv[1]);
     
-    //converting character decimal table into long decimal table
-    for(int i=0; i<strlen(decimal_char); i++){
-        decimal_long[i] = char_to_long(decimal_char[i]);
-    }
+        //converting character decimal table into long decimal table
+        for(int i=0; i<strlen(decimal_char); i++){
+            decimal_long[i] = char_to_long(decimal_char[i]);
+        }
 
-    //converting decimal long table into single decimal long
-    long ddecimal_long=0;
-    for(int i=0; i<strlen(decimal_char); i++){
-        ddecimal_long = ddecimal_long*10 + decimal_long[i];
-    }
+        //converting decimal long table into single decimal long
+        for(int i=0; i<strlen(decimal_char); i++){
+            final_decimal_long = final_decimal_long*10 + decimal_long[i];
+        }
+        break;
 
-
-    //Hexadecimal tp decimal conversion
-    //Max Hex lenght: 8
-    long hexadecimal_long[9];
-    strcpy(hexadecimal_char,argv[4]);
+    case 4:
+        //Hexadecimal to decimal conversion
+        //Max Hex lenght: 8
+        long hexadecimal_long[9];
+        strcpy(hexadecimal_char,argv[1]);
     
-    //converting character hexadecimal table into long hexadecimal table
-    for(int i=0; i<strlen(hexadecimal_char); i++){
-        hexadecimal_long[i] = char_to_long(hexadecimal_char[i]);
+        //converting character hexadecimal table into long hexadecimal table
+        for(int i=0; i<strlen(hexadecimal_char); i++){
+            hexadecimal_long[i] = char_to_long(hexadecimal_char[i]);
+        }
+
+        //converting hexadecimal long table into single decimal long
+        final_decimal_long = nsys_to_decimal(hexadecimal_long, strlen(hexadecimal_char), 16);
+        break;
+
+    default:
+        break;
     }
 
-    //converting hexadecimal long table into single decimal long
-    long hdecimal_long = nsys_to_decimal(hexadecimal_long, strlen(hexadecimal_char), 16);
 
 
+    //returning resault to PHP trought printf
+    switch (data_type)
+    {
+    case 1:
+        
+        printf("%s\n", binary_char);
 
+        decimal_to_nsys(final_decimal_long, octal_char, 12, 8, true);
+        printf("%s\n",octal_char);
 
+        decimal_to_nsys(final_decimal_long, decimal_char, 11, 10, true);
+        printf("%s\n",decimal_char);
 
-//returning resault to PHP trought printf
-printf("%s ",binary_char);
-printf("%i ",bdecimal_long);
-decimal_to_nsys(bdecimal_long, binary_char, 33, 2, true);
-printf("%s\n", binary_char);
+        decimal_to_nsys(final_decimal_long, hexadecimal_char, 9, 16, true);
+        printf("%s\n",hexadecimal_char);
+        break;
 
+    case 2:
+        
+        decimal_to_nsys(final_decimal_long, binary_char, 33, 2, true);
+        printf("%s\n", binary_char);
 
-printf("%s ",octal_char);
-printf("%i ",odecimal_long);
-decimal_to_nsys(odecimal_long, octal_char, 12, 8, true);
-printf("%s\n",octal_char);
+        printf("%s\n",octal_char);
 
+        decimal_to_nsys(final_decimal_long, decimal_char, 11, 10, true);
+        printf("%s\n",decimal_char);
 
-printf("%s ",decimal_char);
-printf("%i ",ddecimal_long);
-decimal_to_nsys(ddecimal_long, decimal_char, 11, 10, true);
-printf("%s\n",decimal_char);
+        decimal_to_nsys(final_decimal_long, hexadecimal_char, 9, 16, true);
+        printf("%s\n",hexadecimal_char);
+        break;
 
-printf("%s ",hexadecimal_char);
-printf("%i ",hdecimal_long);
-decimal_to_nsys(hdecimal_long, hexadecimal_char, 9, 16, true);
-printf("%s\n",hexadecimal_char);
+    case 3:
+        
+        decimal_to_nsys(final_decimal_long, binary_char, 33, 2, true);
+        printf("%s\n", binary_char);
 
-return(0);
+        decimal_to_nsys(final_decimal_long, octal_char, 12, 8, true);
+        printf("%s\n",octal_char);
+
+        printf("%s\n",decimal_char);
+
+        decimal_to_nsys(final_decimal_long, hexadecimal_char, 9, 16, true);
+        printf("%s\n",hexadecimal_char);
+        break;
+
+    case 4:
+        
+        decimal_to_nsys(final_decimal_long, binary_char, 33, 2, true);
+        printf("%s\n", binary_char);
+
+        decimal_to_nsys(final_decimal_long, octal_char, 12, 8, true);
+        printf("%s\n",octal_char);
+
+        decimal_to_nsys(final_decimal_long, decimal_char, 11, 10, true);
+        printf("%s\n",decimal_char);
+
+        printf("%s\n",hexadecimal_char);
+        break;
+    
+    default:
+        break;
+    }
+
 }
 
 
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 
 <?php
+//Turning of warnings on page
+error_reporting(0);
+
 /* script sending data from first cell of form into C script for processing 
 and saving resaults as variable */
 
@@ -12,9 +15,25 @@ if(isset($_POST['Sub'])){
     $value_oct=$_POST['Oct'];
     $value_dec=$_POST['Dec'];
     $value_hex=$_POST['Hex'];
-    exec("calculator.exe $value_bin $value_oct $value_dec $value_hex", $output, $retval );
-}    
-?>
+
+
+    //allowing only one of numeral types to be passed at same time
+    $bin = false;
+    $oct = false;
+    $dec = false;
+    $hex = false;
+
+    $input_type = 0;
+    $value;
+
+    if(!empty($value_bin)) {$bin = true; $input_type = 1; $value = $value_bin;}
+    if(!empty($value_oct)) {$oct = true; $input_type = 2; $value = $value_oct;}
+    if(!empty($value_dec)) {$dec = true; $input_type = 3; $value = $value_dec;}
+    if(!empty($value_hex)) {$hex = true; $input_type = 4; $value = $value_hex;}
+
+    exec("calculator.exe $value $input_type", $output, $retval);
+}?>
+
 
 
 
@@ -28,14 +47,36 @@ if(isset($_POST['Sub'])){
     </head>
     <body>
     
+    
     <form action="index.php" method="post">
-    Bin: <input type="text" name="Bin"><br>
-    Oct: <input type="text" name="Oct"><br>
-    Dec: <input type="text" name="Dec"><br>
-    Hex: <input type="text" name="Hex"><br>
+
+    Bin: <input type="text" name="Bin"
+    placeholder=<?php 
+    if(empty($value_bin)) {Echo("00000000000000000000000000000000");}
+    else {Echo($value_bin);}?>
+    <?php if($oct||$dec||$hex) {Echo("Disabled");} ?>><br>
+
+    Oct: <input type="text" name="Oct"
+    placeholder=<?php
+    if(empty($value_oct)) {Echo("00000000000");}
+    else {Echo($value_oct);}?>
+    <?php if($bin||$dec||$hex) {Echo("Disabled");} ?>><br>
+
+    Dec: <input type="text" name="Dec"
+    placeholder=<?php
+    if(empty($value_dec)) {Echo("0000000000");}
+    else {Echo($value_dec);}?>
+    <?php if($bin||$oct||$hex) {Echo("Disabled");} ?>><br>
+
+    Hex: <input type="text" name="Hex"
+    placeholder=<?php
+    if(empty($value_hex)) {Echo("00000000");}
+    else {Echo($value_hex);}?>
+    <?php if($bin||$oct||$dec) {Echo("Disabled");} ?>><br>
+
     <input type="submit" name="Sub">
     </form>
-
+    
 
     
     <?php
