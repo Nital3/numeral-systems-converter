e5ed93f386f3eb0368e3cf8d02ee666c13ab13de
Nital <daniel.siwiaszczyk0@gmail.com>
2024-01-16 23:52:22 +0100

    form tweaks
    Slight change to how locking form works after submiting

--- index.php
+++ index.php
@@ -20,12 +20,8 @@ if(isset($_POST['Sub'])){
     if($_POST['signed'] == '1') {$signed = '1';}
     else {$signed = '0';}
 
-
-    //allowing only one of numeral types to be passed at same time
-    $bin = false;
-    $oct = false;
-    $dec = false;
-    $hex = false;
+    //form lock default setting
+    $lock = false;
 
     $input_type = 0;
     $value;
@@ -35,7 +31,7 @@ if(isset($_POST['Sub'])){
             echo '<script>alert("Invalid input")</script>';
         }
         else {
-            $bin = true;
+            $lock = true;
             exec("calculator.exe $value_bin 1 $signed", $output, $retval);
          }
     }
@@ -46,7 +42,7 @@ if(isset($_POST['Sub'])){
             echo '<script>alert("Invalid input")</script>';
         }
         else {
-            $oct = true;
+            $lock = true;
             exec("calculator.exe $value_oct 2 $signed", $output, $retval);
          }
     }
@@ -56,7 +52,7 @@ if(isset($_POST['Sub'])){
             echo '<script>alert("Invalid input")</script>';
         }
         else {
-            $dec = true;
+            $lock = true;
             exec("calculator.exe $value_dec 3 $signed", $output, $retval);
          }
     }
@@ -67,7 +63,7 @@ if(isset($_POST['Sub'])){
             echo '<script>alert("Invalid input")</script>';
         }
         else {
-            $hex = true;
+            $lock = true;
             exec("calculator.exe $value_hex 4 $signed", $output, $retval);
          }
     }
@@ -104,28 +100,29 @@ if(isset($_POST['Sub'])){
             placeholder=<?php 
             if(empty($value_bin)) {Echo("00000000000000000000000000000000");}
             else {Echo($value_bin);}?>
-            <?php if($oct||$dec||$hex) {Echo("Disabled");} ?>><br>
+            <?php if($lock) {Echo("Disabled");} ?>><br>
             
-            <input type="checkbox" id="signed" name="signed" value="1">
+            <input type="checkbox" id="signed" name="signed" value="1"
+            <?php if($lock) {Echo("Disabled");} ?>>
             <label for="signed"> Signed</label><br>
 
             Oct: <input type="text" name="Oct" max="37777777777" maxlength="10"
             placeholder=<?php
             if(empty($value_oct)) {Echo("00000000000");}
             else {Echo($value_oct);}?>
-            <?php if($bin||$dec||$hex) {Echo("Disabled");} ?>><br>
+            <?php if($lock) {Echo("Disabled");} ?>><br>
 
             Dec: <input type="text" name="Dec" maxlength="9"
             placeholder=<?php
             if(empty($value_dec)) {Echo("0000000000");}
             else {Echo($value_dec);}?>
-            <?php if($bin||$oct||$hex) {Echo("Disabled");} ?>><br>
+            <?php if($lock) {Echo("Disabled");} ?>><br>
 
             Hex: <input type="text" name="Hex" maxlength="7"
             placeholder=<?php
             if(empty($value_hex)) {Echo("00000000");}
             else {Echo($value_hex);}?>
-            <?php if($bin||$oct||$dec) {Echo("Disabled");} ?>><br>
+            <?php if($lock) {Echo("Disabled");} ?>><br>
 
             <input type="submit" value=<?php if(empty($value_bin)) {echo("Submit");} else{echo("Clear");}?> name="Sub">
             </form> 
